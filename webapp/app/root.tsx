477c138685edcc3645448e0a462db74f16e3aec4
Andrin Meier <andrin.meier@hispeed.ch>
2022-03-04 13:03:26 +0100

    webapp: add favicons / logo
    The color palette was chosen using machine learning (huemint.com), the logo itself is hand made and designed with Affinity Designer (romme.svg).

--- webapp/app/root.tsx
+++ webapp/app/root.tsx
@@ -18,6 +18,13 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#fe4e4d" />
+        <meta name="msapplication-TileColor" content="#f9feff" />
+        <meta name="theme-color" content="#ffffff" />
         <Meta />
         <Links />
       </head>
