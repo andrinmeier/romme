5af3c3e91eb3a7c1da329c397fad88b8ec48aafe
Andrin Meier <andrin.meier@hispeed.ch>
2022-03-05 09:12:58 +0100

    server: switch from builderpack to simple dockerfile
    The deployment wasn't working before and using a simple Dockerfile seems easier to debug the problems.

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,6 +6,6 @@ let app: express.Application = express();
 Server.buildServices(app, AuthenticationService);
 
 const port = process.env.port || 8080;
-app.listen(port, function () {
+app.listen(Number(port), '0.0.0.0', function () {
     console.log('Rest Server listening on port ' + port);
 });
