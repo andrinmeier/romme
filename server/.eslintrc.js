976f0cd5fa0c0ce97e01e6f7c15c7fd99c2053ca
Andrin Meier <andrin.meier@hispeed.ch>
2022-03-06 15:52:18 +0100

    code quality: fix line endings for prettier

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -9,6 +9,11 @@ module.exports = {
         "prettier"
     ],
     rules: {
-        "prettier/prettier": 2 // Means error
+        "prettier/prettier": [
+            "error",
+            {
+                endOfLine: "auto"
+            }
+        ]
     }
 };
